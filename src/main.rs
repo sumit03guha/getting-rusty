c89af426318169a7446b1b434119fd8423ae94c3
Sumit Guha <sumit03guha@gmail.com>
2023-01-11 23:36:45 +0530

    understood shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name?");
+
+    let mut name = String::new();
+    let greeting = "Nice to meet you";
+    io::stdin().read_line(&mut name)
+        .expect("Didn't receive input");
+
+    println!("Hello, {}! {}", name.trim_end(), greeting);
+
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.141592654;
+
+    // shadowing
+    let age = "47";
+    let mut age:u32 = age.trim().parse()
+        .expect("Not a number");
+
+    age = age + 1;
+
+    print!("I am {} and I want {}", age, ONE_MIL);
+
+
 }
